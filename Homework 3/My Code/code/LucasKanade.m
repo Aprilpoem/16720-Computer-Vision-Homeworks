b063980e1747f6e728bf7c36a4be13e33bcd2404
Azarakhsh Keipour <keipour@gmail.com>
2016-02-25 21:19:44 -0500

    HW 3: Questions 1.2 and 1.3 implemented

--- Homework 3/My Code/code/LucasKanade.m
+++ Homework 3/My Code/code/LucasKanade.m
@@ -1,6 +1,37 @@
-
 function [u, v] = LucasKanade(It, It1, rect)
+It = im2double(It);
+It1 = im2double(It1);
 
+% Initialize parameters
+tol = 0.1;
+p = [0 0]';
+deltap = 2 * [tol tol]';
 
-end 
+% Isolate the template that we want to track
+[X, Y] = meshgrid(rect(1) : rect(3), rect(2) : rect(4));
+T = interp2(It, X, Y);
+
+% Lucas-Kanade iterative algorithm
+while any(abs(deltap) > tol)
+    % Isolate current part of the image we want to compare with template
+    [X, Y] = meshgrid((rect(1) : rect(3)) + p(1), ...
+        (rect(2) : rect(4)) + p(2));
+    I = interp2(It1, X, Y);
+
+    % Calculate the gradient
+    [Ix, Iy] = gradient(I);
     
+    % Calculate delta p
+    A = [Ix(:) Iy(:)]' * [Ix(:) Iy(:)];
+    b = -[Ix(:) Iy(:)]' * (I(:) - T(:));
+    deltap = A \ b;
+    
+    % Update p
+    p = p + deltap;
+end
+
+% Update u and v
+u = p(1);
+v = p(2);
+
+end
--- Homework 3/My Code/code/testCarSequence.m
+++ Homework 3/My Code/code/testCarSequence.m
@@ -1,9 +1,59 @@
+% Close all open figure windows
+close all;
 
+% Load frames
 load(fullfile('..','data','carseq.mat')); % variable name = frames. 
 
+% Initialize position of the car that we want to track
+rect = [60, 117, 146, 152];
 
+% Frames that should be reported
+reportFrames = [1 100 200 300 400];
 
+% Initialization
+rects = zeros(size(frames, 3), 4);
 
+% Track the car in the consecutive frames
+for i = 1 : size(frames, 3)
+    tic;
+    
+    % Calculate new position of the car
+    if i ~= 1 
+        [u, v] = LucasKanade(squeeze(frames(:, :, i-1)), ...
+            squeeze(frames(:, :, i)), rect);
+        rect = rect + [u, v, u, v];
+    end
+    
+    % Draw current frame
+    imshow(frames(:, :, i));
+    hold on
+    
+    % Draw the rectangle
+    rectangle('Position', [rect(1), rect(2), rect(3) - rect(1), ...
+        rect(4) - rect(2)], 'LineWidth', 2, 'EdgeColor', 'y');
+    hold off
 
-% save the rects
+    % Check if the frame should be reported as a result
+    j = find(reportFrames == i);
+    if ~isempty(j)
+        % Set title
+        title(sprintf('%d (%0.3f milliseconds)', i, toc * 1000));
+        
+        % Save the image
+        path = fullfile('..','results', sprintf('q3_2_frame_%d', reportFrames(j(1))));
+        print(path, '-djpeg');
+    end
+    
+    % Set the image title
+    title(sprintf('Frame %d of %d', i, size(frames, 3)));
+
+    % Save new position in a variable
+    rects(i, :) = rect;
+    
+    % Pause for a few milliseconds
+    pause(0.01);
+
+end
+
+% Save the rects
 save(fullfile('..','results','carseqrects.mat'),'rects');
