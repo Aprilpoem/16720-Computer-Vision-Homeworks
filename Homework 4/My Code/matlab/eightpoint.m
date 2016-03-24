2dbf0d8ad519d00c2f2cf3014682c632168c5b8b
Azarakhsh Keipour <keipour@gmail.com>
2016-03-23 22:29:27 -0400

    HW 4: Q2.1 done

--- Homework 4/My Code/matlab/eightpoint.m
+++ Homework 4/My Code/matlab/eightpoint.m
@@ -11,5 +11,34 @@ function [ F ] = eightpoint( pts1, pts2, M )
 
 %     Write F and display the output of displayEpipolarF in your writeup
 
-end
+%% Scale the data by factor M
+pts1 = pts1 / M;
+pts2 = pts2 / M;
 
+%% Generate the matrices A and B in AF = B
+A = [pts1(:, 1) .* pts2(:, 1), pts1(:, 1) .* pts2(:, 2), pts1(:, 1), ...
+     pts1(:, 2) .* pts2(:, 1), pts1(:, 2) .* pts2(:, 2), pts1(:, 2), ...
+     pts2(:, 1), pts2(:, 2)];
+
+B = -ones(size(pts1, 1), 1);
+
+%% Calculate the Fundamental matrix F
+Fs = A \ B; 
+F = reshape([Fs; 1], [3 3]);
+
+%% Enforce the singularity condition on F
+[U, S, V] = svd(F);
+S(3, 3) = 0;
+F = U * S * V';
+
+%% Refine the calculated fundamental matrix
+F = refineF(F, pts1, pts2);
+
+%% Unscale the fundamental matrix F
+% Create scaling matrix T
+T = diag([1 / M, 1 / M, 1]);
+
+% Unscale F
+F = T' * F * T;
+
+end
