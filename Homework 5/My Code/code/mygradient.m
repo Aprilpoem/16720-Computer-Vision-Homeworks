64a7639b5a8b2807aee02578de20f786f78f0ce5
Azarakhsh Keipour <keipour@gmail.com>
2016-03-26 14:11:27 -0400

    HW 5: Q 1.1 done

--- Homework 5/My Code/code/mygradient.m
+++ Homework 5/My Code/code/mygradient.m
@@ -3,4 +3,26 @@ function [mag,ori] = mygradient(I)
 % compute image gradient magnitude and orientation at each pixel
 %
 
+%% Convert image to double
+I = im2double(I);
 
+%% Create the gradient filter
+
+% filt = fspecial('sobel');
+filt = [    1     2     1
+            0     0     0
+           -1    -2    -1   ];
+
+% filt = fspecial('prewitt');
+% filt = [    1     1     1
+%             0     0     0
+%            -1    -1    -1   ];
+
+%% Find the horizontal and vertical gradients
+gradx = imfilter(I, filt', 'replicate', 'same');
+grady = imfilter(I, filt, 'replicate', 'same');
+
+%% Compute the magnitude and orientation of the gradient
+grad = gradx + grady * 1i;
+mag = abs(grad);
+ori = angle(grad);
---
+++ Homework 5/My Code/code/test.m
@@ -0,0 +1,34 @@
+clc;
+close all;
+
+% List of test images
+images = [ 'test0.jpg'
+           'test1.jpg'  ];
+
+for i = 1 : size(images, 1)
+
+    % Load the test image
+    I = imread(['../data/' images(i, :)]);
+
+    % Convert the test image to grayscale
+    I = rgb2gray(I);
+
+    % Compute gradient magnitude and orientation images
+    [mag, ori] = mygradient(I);
+
+    % Visualize the magnitude
+    figure;
+    colormap('gray');
+    imagesc(mag);
+    axis off;
+    title(['Gradient Magnitude of ' images(i, :)]);
+    
+    
+    % Visualize the orientation
+    figure;
+    colormap('gray');
+    imagesc(ori);
+    axis off;
+    title(['Gradient Orientation of ' images(i, :)]);
+    
+end
