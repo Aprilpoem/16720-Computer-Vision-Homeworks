a782188c397df012e911be5d8f076e6a595bcce1
Azarakhsh Keipour <keipour@gmail.com>
2016-03-27 00:24:58 -0400

    HW 5: Q 1.2 (histogram of gradient orientations) done

--- Homework 5/My Code/code/hog.m
+++ Homework 5/My Code/code/hog.m
@@ -11,5 +11,50 @@ function ohist = hog(I)
 %   NOTE: Don't divide by 0! If there are no edges in a block (ie. this counts sums to 0 for the block) then just leave all the values 0. 
 % TODO
 
+%% Compute the gradient magnitude and orientation
+[mag, ori] = mygradient(I);
 
+%% Initialize parameters
 
+% Block size
+BlockSize = 8;
+
+% Threshold for considering a pixel as an edge
+thresh = 0.1 * max(mag(:));
+
+% Number of orientation bins
+Nbins = 9;
+
+% Create orientation bins
+Bins = linspace(-pi / 2, pi / 2, Nbins + 1);
+
+% Divide the image orientations in to blocks
+OriBlocks = im2col(ori, [BlockSize BlockSize], 'distinct');
+
+% Create a mask of the cells that are considered as an edge
+MaskBlocks = im2col(mag >= thresh, [BlockSize BlockSize], 'distinct');
+
+% Allocate space for the output
+ohist = zeros(size(OriBlocks, 2), Nbins);
+
+%% Calculate gradient orientation histograms
+for i = 1 : Nbins
+    criteria = MaskBlocks & OriBlocks > Bins(i) & OriBlocks <= Bins(i + 1);
+    ohist(:, i) = sum(criteria)';
+end
+
+%% Normalize so that sum over orientation bins is 1 for each block
+
+% Calculate sums for each block
+histSum = sum(ohist, 2);
+
+% Convert the sums vestor to a matrix of the size of the histogram
+histSumMat = repmat(histSum, 1, Nbins);
+
+% Normalize the histogram
+ohist(histSum > 0, :) = ohist(histSum > 0, :) ./ histSumMat(histSum > 0, :);
+
+%% Reshape the histogram to the desired output dimensions
+BlockRows = ceil(size(I, 1) / BlockSize);
+BlockCols = ceil(size(I, 2) / BlockSize);
+ohist = reshape(ohist(:), BlockRows, BlockCols, Nbins);
---
+++ Homework 5/My Code/code/test_q1_2.m
@@ -0,0 +1,26 @@
+clc;
+close all;
+
+% List of test images
+images = [ 'test0.jpg'
+           'test1.jpg'  ];
+
+for i = 1 : size(images, 1)
+
+    % Load the test image
+    I = imread(['../data/' images(i, :)]);
+
+    % Convert the test image to grayscale
+    I = rgb2gray(I);
+
+    % Compute the histogram of gradient orientations
+    ohist = hog(I);
+
+    % Visualize the HoG
+    figure;
+    % colormap('gray');
+    imagesc(hogdraw(ohist));
+    axis off;
+    title(['Histogram of gradient otientations for ' images(i, :)]);
+
+end
