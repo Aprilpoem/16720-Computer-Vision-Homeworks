139aaaf57bb5916cdf5aa6628998e978b4026db4
Azarakhsh Keipour <keipour@gmail.com>
2016-04-09 20:47:17 -0400

    HW 5: Q 2.4 Now the template in LDA method is calculated channel by channel.

--- Homework 5/My Code/code/tl_lda.m
+++ Homework 5/My Code/code/tl_lda.m
@@ -1,7 +1,7 @@
 function template = tl_lda(template_images_pos, template_images_neg, lambda)
 % input:
-%     template_images_pos - a cell array, each one contains [16 x 16 x 9] matrix
-%     template_images_neg - a cell array, each one contains [16 x 16 x 9] matrix
+%     template_images_pos - a cell array, each one contains [128 x 128] matrix
+%     template_images_neg - a cell array, each one contains [128 x 128] matrix
 %     lambda - parameter for lda
 % output:
 %     template - [16 x 16 x 9] matrix 
@@ -13,17 +13,26 @@ Mu_neg = tl_pos(template_images_neg);
 % (i.e. deltaMu = Mu_pos - Mu_neg)
 delta_mu = tl_pos(template_images_pos) - Mu_neg;
 
-% Calculate the the covariance matrix of negative examples
-Sigma_neg = zeros(numel(delta_mu));
-for i = 1 : numel(template_images_neg)
-    Neg_diff = hog(template_images_neg{i}) - Mu_neg;
-    Sigma_neg = Sigma_neg + Neg_diff(:) * Neg_diff(:)';
-end
-Sigma_neg = Sigma_neg / numel(template_images_neg);
-Sigma_neg = Sigma_neg + lambda * eye(size(Sigma_neg));
+% Initialize the output template
+template = zeros(size(delta_mu));
 
-% Calculate the final template and reshape to the required size
-template = Sigma_neg \ delta_mu(:);
-template = reshape(template, size(delta_mu));
+% Calculate the template channel-by-channel
+for channel = 1 : size(delta_mu, 3)
+
+    % Calculate the covariance matrix of negative examples
+    Sigma_neg = zeros(size(delta_mu, 1), size(delta_mu, 2));
+    for i = 1 : numel(template_images_neg)
+        sample_hog = hog(template_images_neg{i});
+        Neg_diff = sample_hog(:, :, channel) - Mu_neg(:, :, channel);
+        Sigma_neg = Sigma_neg + Neg_diff * Neg_diff';
+    end
+    Sigma_neg = Sigma_neg / numel(template_images_neg);
+    Sigma_neg = Sigma_neg + lambda * eye(size(Sigma_neg));
+
+    % Calculate the final template for the current channel
+    delta_mu_channel = delta_mu(:, :, channel);
+    template(:, :, channel) = Sigma_neg \ delta_mu_channel;
+
+end
 
 end
--- Homework 5/My Code/code/tl_pos_neg.m
+++ Homework 5/My Code/code/tl_pos_neg.m
@@ -1,7 +1,7 @@
 function template = tl_pos_neg(template_images_pos, template_images_neg)
 % input:
-%     template_images_pos - a cell array, each one contains [16 x 16 x 9] matrix
-%     template_images_neg - a cell array, each one contains [16 x 16 x 9] matrix
+%     template_images_pos - a cell array, each one contains [128 x 128] matrix
+%     template_images_neg - a cell array, each one contains [128 x 128] matrix
 % output:
 %     template - [16 x 16 x 9] matrix 
 
